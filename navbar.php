9c900675074d84526a344c4722321d0c9cea3523
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-10-01 16:53:43 +0200

    fixed issue mobilefriendlyness
    Fixed the issue with the page didnt scale down correctly while user on mobile or small display. Now its responsive and scale down correct.

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <div class="nav-container">
     <nav class="nav-item">
-        <a href="index.php"><img class="table-icon" src="assets/home.svg" style="display:inline;"/><p style="display:inline;">Home</p></a>
-        <a href="about.php"><img class="table-icon" src="assets/question.svg" style="display:inline;"/><p style="display:inline;">About</p></a>
-        <a href="tabledata.php"><img class="table-icon" src="assets/db.svg" style="display:inline;"/><p style="display:inline;">statistics</p></a>
+        <a class="nav-sideway-box" href="index.php"><img class="table-icon" src="assets/home.svg"/><p style="display:inline;">Home</p></a>
+        <a class="nav-sideway-box" href="about.php"><img class="table-icon" src="assets/question.svg"/><p style="display:inline;">About</p></a>
+        <a class="nav-sideway-box" href="tabledata.php"><img class="table-icon" src="assets/db.svg"/><p style="display:inline;">statistics</p></a>
     </nav>
 </div>            
