5bb0441c832918feedc39746b021cbe49c9d6c40
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2023-10-03 17:11:25 +0200

    unique ciys addes

--- cityValues.php
+++ cityValues.php
@@ -5,11 +5,22 @@ __DIR__ . require 'citydata.php'; ?>
 <main>
     <div class="container">
         <div class="city-container">
-        <?php        
-        foreach ($teams as $key => $value) {?>
-            <p class="city-item"><?= $citys[$i] . " is the city of " . $key ?></p><br><?php $i++ ;
-            }
-        ?>
+            <div class="Info-box">
+            <p>In the array there are 7 teams from 5 different citys</p>
+            <p>The different citys are:</p>
+            </div>
+            <div class="city-box"><?php
+            foreach ($city_array_sorted as $city){?>
+                <p><?= $city;?></p><br>
+                <?php
+            }?>
+            </div>
+            <p><u>And:</u></p>
+            <?php        
+            foreach ($teams as $key => $value) {?>
+                <p class="city-item"><?= $citys[$i] . " is the city of " . $key ?></p><br><?php $i++ ;
+                }
+            ?>
         </div>
     </div>
 </main>
--- citydata.php
+++ citydata.php
@@ -1,11 +1,8 @@
 <?php
+__DIR__. require 'data.php';
 $i = 0;
-$citys = [
-    'London',
-    'Brimingham',
-    'Manchester',
-    'Newcastle',
-    'London',
-    'London',
-    'Brighton'
-];
+$citys = [];//ny tom array som fylls med alla lagens städer ifrån teamsarrayen
+foreach ($teams as $city){
+    $citys[] = $city['city'];
+}
+$city_array_sorted = array_unique($citys);//Ny tom array som fylls med enbart unika värden från citysarrayen
